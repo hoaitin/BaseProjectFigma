c3aecccec68c8ac1acbcab3cc5b2253368391270
hoaitin <lucvy221722@gmail.com>
2024-01-10 10:20:48 +0700

    tinnguyen
    update

--- Sources/App/Views/OB2CollectionViewCell.swift
+++ Sources/App/Views/OB2CollectionViewCell.swift
@@ -14,15 +14,7 @@ class OB2CollectionViewCell: UICollectionViewCell {
     private lazy var backgroundImageView = UIImageView()
     private lazy var navbarTextlabel = GradientLabel()
     private lazy var continueButton = ContinueButton()
-    private lazy var categoriesCollectionview = UICollectionView()
-    private var categories: [Category] = [
-     Category(image: "man", name: "Man"),
-     Category(image: "woman", name: "Woman"),
-     Category(image: "landscape", name: "Landscape"),
-     Category(image: "animal", name: "Animal"),
-     Category(image: "supper_car", name: "Supper Car"),
-     Category(image: "mecha_robot", name: "Mecha Robot"),
-    ]
+    private lazy var contentImage = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -49,13 +41,16 @@ class OB2CollectionViewCell: UICollectionViewCell {
         navbarTextlabel.font = .boldSystemFont(ofSize: 36)
         navbarTextlabel.numberOfLines = 2
         
-        self.setCategoriesCollection()
+        contentImage.image = UIImage(named: "image_categories")
+        contentImage.contentMode  = .scaleAspectFill
+        
+      
     }
     
     func setUpConstraints(){
         contentView.addSubview(backgroundImageView)
         contentView.addSubview(navbarTextlabel)
-        contentView.addSubview(categoriesCollectionview)
+        contentView.addSubview(contentImage)
         
         
         backgroundImageView.snp.makeConstraints{
@@ -69,44 +64,10 @@ class OB2CollectionViewCell: UICollectionViewCell {
             $0.size.equalTo(CGSize(width: 250, height: 92))
         }
         
-        categoriesCollectionview.snp.makeConstraints{
+        contentImage.snp.makeConstraints{
             $0.center.equalToSuperview()
             $0.size.equalTo(CGSize(width: contentView.frame.width - 40, height: 254))
         }
     }
-    
-}
-extension OB2CollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
-    
-    func setCategoriesCollection(){
-        
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 6)
-        layout.itemSize = CGSize(width: 100, height: 120)
-        layout.scrollDirection = .vertical
-        self.categoriesCollectionview = UICollectionView(frame: contentView.frame, collectionViewLayout: layout)
-        self.categoriesCollectionview.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.id)
-        self.categoriesCollectionview.backgroundColor =  UIColor(red: 1, green: 1, blue: 1, alpha: 0)
-        self.categoriesCollectionview.isScrollEnabled = false
-        self.categoriesCollectionview.dataSource = self
-        self.categoriesCollectionview.delegate = self
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.id, for: indexPath) as? CategoryCollectionViewCell else {
-            return .init()
-        }
-        
-        let category = categories[indexPath.row]
-        cell.setData(image: category.image, text: category.name)
-        
-        return cell
-    }
-    
     
 }
